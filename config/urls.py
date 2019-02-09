18e85dbcb34a2fcce01a5c9dcb943973bae0a59d
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-09 14:14:23 +0100

    hello world

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path('rest/', include('rest.urls'))
+    path('rest/', include('rest.urls')),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
